88c43b4c46cf91f2df363e89c320a35ad4ef94f4
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-10-11 19:03:05 +0530

    using purgecss to emove unused css

--- Workspace/01-mini-project/tailwind.config.js
+++ Workspace/01-mini-project/tailwind.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  purge: [],
+  /*
+    If you replace “ enabled: true ” with “enabled: false”, then it no longer works. You should only
+    set it to “true” if you want to publish the project.
+    You should also temporarily set “enabled” back to false if you want to make subsequent
+    changes to the tailwind.css or the Tailwind configuration
+    (new plugins or changes in the tailwind.config.js ).
+    Background info:
+    https://tailwindcss.com/docs/optimizing-for-production
+  */
+  purge: {
+    enabled: true, // set to false
+    content: ['./dist/**/*.html'],
+  },
   // 'media' - means it it set darkMode based on device setting.
   darkMode: 'class', // false or 'media' or 'class'
   theme: {
